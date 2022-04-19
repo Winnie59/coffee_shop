e582d710fa780aa7c09d121adb4847bf43c7c3a8
winnie59 <kwang_kwah_noi@yahoo.com>
2022-04-18 21:13:54 -0400

    start

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -64,6 +65,7 @@ export default function Home() {
           </span>
         </a>
       </footer>
+    
     </div>
   )
 }
